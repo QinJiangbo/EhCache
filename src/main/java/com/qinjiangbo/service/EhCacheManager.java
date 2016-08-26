f4067ad37cbc27dd8749da3b6e9850242393aa2c
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-08-26 23:49:30 +0800

    add ehcache cache manager

--- src/main/java/com/qinjiangbo/service/EhCacheManager.java
+++ src/main/java/com/qinjiangbo/service/EhCacheManager.java
@@ -1,7 +1,14 @@
 package com.qinjiangbo.service;
 
+import net.sf.ehcache.CacheManager;
+
 /**
  * Created by Richard on 8/26/16.
  */
 public class EhCacheManager {
+
+    public CacheManager findCacheManager() {
+        CacheManager.newInstance("ehcache.xml");
+        return CacheManager.create("ehcache.xml");
+    }
 }
